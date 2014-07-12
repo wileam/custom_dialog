f4472441aa1da8cd127559618b0fb2efc00e0ad1
Mengyuan Wu <wmy1011@gmail.com>
2014-07-12 17:51:13 +0800

    style dialog demo

--- js/window.js
+++ js/window.js
@@ -9,17 +9,24 @@ define(['jquery'],function($){
             $alertBox.appendTo('body');
             $alertBox.html(msg);
 
-            var $closeBtn = $('<input type="button" value="Close" class="closeBtn">');
+            var $closeBtn = $('<input type="button" value="Close" class="close-btn">');
             $closeBtn.appendTo($alertBox);
 
-            if(handle) {
-                $closeBtn.on('click',function(event) {
-                    event.preventDefault();
-                    /* Act on the event */
-                    handle();
-                    $alertBox.remove();
-                });
-            }
+            // if(handle) {
+            //     $closeBtn.on('click',function(event) {
+            //         event.preventDefault();
+            //         /* Act on the event */
+            //         handle();
+            //         $alertBox.remove();
+            //     });
+            // }
+
+            $closeBtn.on('click', function(e) {
+                event.preventDefault();
+                /* Act on the event */
+                handle && handle();
+                $alertBox.remove();
+            });
         },
         comfirm: function(){},
         prompt: function(){}
